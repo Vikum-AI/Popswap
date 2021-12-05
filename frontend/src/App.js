f95ce4290c053775b6c9ddc938cf3832edaf518f
Saka Ricky <sakaricky91@gmail.com>
2021-12-05 23:23:39 +0100

    feat: created the swap exhange form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/header'
+import Header from './Components/header';
+import SwapCoin from './Components/SwapCoin/SwapCoin';
 
 function App() {
   return (
     <div className="App">
       
       <Header /> 
-
+      <SwapCoin />
     </div>
   );
 }
